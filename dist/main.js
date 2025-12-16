6a1a5fb05aff857aa04597e4c212f74079527032
Mansour <bm6276492@gmail.com>
2025-12-16 23:46:28 +0200

    add ts config + app.module

---
+++ dist/main.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const common_1 = require("@nestjs/common");
+const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: true,
+        credentials: true,
+    });
+    app.useGlobalPipes(new common_1.ValidationPipe());
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Portfolio API')
+        .setDescription('API documentation for Portfolio Backend')
+        .setVersion('1.0')
+        .addBearerAuth({
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+    }, 'access-token')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api/docs', app, document);
+    const port = process.env.PORT || 3001;
+    await app.listen(port);
+    console.log(`Application is running on: http://localhost:${port}`);
+    console.log(`Swagger docs available at: http://localhost:${port}/api/docs`);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
